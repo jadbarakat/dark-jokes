bb08a821976525b2237fb05577c15586fc055362
Jad Barakat <jad.barakat@hotmail.com>
2023-09-07 21:05:11 +1000

    incremented Android version code

--- App.js
+++ App.js
@@ -1,10 +1,29 @@
-import { Suspense } from "react";
+import { Suspense, useEffect } from "react";
 import { ActivityIndicator } from "react-native";
 import { DarkJokesApp } from "./src/DarkJokesApp";
 import { useTheme } from "@react-navigation/native";
 
+import * as Updates from "expo-updates";
+
 export default function App() {
   const { colors } = useTheme();
+
+  useEffect(() => {
+    onFetchUpdateAsync();
+  }, []);
+
+  async function onFetchUpdateAsync() {
+    try {
+      const update = await Updates.checkForUpdateAsync();
+
+      if (update.isAvailable) {
+        await Updates.fetchUpdateAsync();
+        await Updates.reloadAsync();
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  }
 
   return (
     <Suspense fallback={<ActivityIndicator color={colors.text} size="large" />}>
